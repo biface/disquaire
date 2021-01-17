71ddb872923b32e3cca99761f5d1517774fcba4b
biface <fabrice.moutte.wn@gmail.com>
2021-01-17 17:41:17 +0100

    Models and data (P2C4)

--- disquaire_project/store/urls.py
+++ disquaire_project/store/urls.py
@@ -18,7 +18,3 @@ from django.conf import settings
 from django.conf.urls import include, url
 from django.contrib import admin
 from django.urls import path
-
-from . import views
-
-urlpatterns = [url(r'^$', views.listing)]

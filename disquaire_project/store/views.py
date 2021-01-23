03b0445c25bb4378b900b85021c4b628e7e30f22
biface <fabrice.moutte.wn@gmail.com>
2021-01-23 17:09:23 +0100

    Templates (P2C5)

--- disquaire_project/store/views.py
+++ disquaire_project/store/views.py
@@ -1,8 +1,11 @@
 # from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 # Create your views here.
 
 def index(request):
     message = "Salut, tout le monde !"
-    return HttpResponse(message)
+    template = loader.get_template('store/index.html')
+    return HttpResponse(template.render(request=request))
+

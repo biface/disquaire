424b907230a823fd36203255b45e1471803c299e
biface <fabrice.moutte.wn@gmail.com>
2021-01-31 17:05:18 +0100

    HTML and vews (P3 End) Using render instead of HTTP Response

--- disquaire_project/store/urls.py
+++ disquaire_project/store/urls.py
@@ -14,7 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-from django.conf import settings
-from django.conf.urls import include, url
-from django.contrib import admin
-from django.urls import path
+
+from django.conf.urls import url
+
+from . import views
+
+app_name = 'store'
+
+urlpatterns = [
+    url(r'^$', views.listing, name='listing'),
+    url(r'^(?P<album_id>[0-9]+)/$', views.detail, name='detail'),
+    url(r'^search/$', views.search, name='search'),
+]

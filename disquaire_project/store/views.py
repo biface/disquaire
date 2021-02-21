8a4d6bb8952753bd924ae391192bdcce424a7317
biface <fabrice.moutte.wn@gmail.com>
2021-02-21 13:08:01 +0100

    P4C2 - ModelForm and ErrorLists

--- disquaire_project/store/views.py
+++ disquaire_project/store/views.py
@@ -50,7 +50,7 @@
 
 from django.shortcuts import render, get_object_or_404
 from .models import Album, Artist, Contact, Booking
-from .forms import ContactForm
+from .forms import ContactForm, ParagraphErrorList
 
 
 def index(request):
@@ -78,7 +78,7 @@ def detail(request, album_id):
         'thumbnail': album.picture
     }
     if request.method == 'POST':
-        form = ContactForm(request.POST)
+        form = ContactForm(request.POST, error_class=ParagraphErrorList)
         if form.is_valid():
             email = form.cleaned_data['email']
             name = form.cleaned_data['name']
